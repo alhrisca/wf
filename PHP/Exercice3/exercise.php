f216ca51147da66ec0e8e9f693ce4b8e87e71aa9
alhrisca <alhrisca@gmail.com>
2018-10-19 12:26:00 +0200

    solved

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -2,3 +2,33 @@
 
 $input;
 $winner;
+//Algorithm
+/*
+1. declare brother A and B
+2. for each row in the input
+explode the row as brother A card and brother B card
+
+3. If the brother A card is upper than the brother B card add a
+point to the brother A, else if the brother B card is upper, give a point to brother B
+4. Set the winner as greater brother value
+*/
+
+// declare variables for the brothers
+$A = 0;
+$B = 0;
+//giving points to one that has higher value
+foreach ($input as $value){
+    if ($value[0] > $value[1]){
+        $A++;
+    } else {
+        $B++;
+    }
+    //declare the winner
+    if($A > $B){
+        $winner = 'A';
+    } else{
+        $winner = 'B';
+    }
+};
+
+?>
