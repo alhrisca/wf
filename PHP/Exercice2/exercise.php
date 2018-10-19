dc41a8c989cf26373b109742cea033bf2243b96e
alhrisca <alhrisca@gmail.com>
2018-10-19 10:34:14 +0200

    Solved

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,10 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+
+$mid = floor(strlen($password)/2);
+$firstPart = substr($password, 0, $mid);
+$lastPart = substr($password, $mid, strlen($password)-1);
+
+$saltedPassword = $firstPart . $salt . $lastPart;
+?>
