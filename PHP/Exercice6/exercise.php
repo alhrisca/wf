32c091cc4fd1deb1cb4f9546a84eb43a0756aa01
alhrisca <alhrisca@gmail.com>
2018-10-22 14:42:20 +0200

    Exercise solved (2ways)

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -8,19 +8,29 @@ function easterReverse(){
     
     fseek($res,floor(strlen($fileContent) / 2),SEEK_SET);
     fwrite($res,$correctContent);
+    
+    fclose($res);
     echo $res;
     
     
-    /*for($i=floor(strlen($fileContent) / 2); strlen($fileContent); $i--){
-       $reverseContent.=
-       fseek('file.txt',-1,SEEK_CUR);
-    }*/
-
-    //fseek('file.txt',floor(strlen($fileContent) / 2),SEEK_END);
-    
-    
-    //echo $fileContent;
-    //fwrite('file.txt',);
+  /*
+   * //In a function named easterReverse
+   * function easterReverse($filePath){
+   * // I assume a file path as first parameter
+   * //Get the full file content
+   * $content = file_get_contents($filePath);
+   * //divide the file content by 2 using floor(strlen($fileContent)/2;
+   * $secondPart = substr($content, floor(strlen($content) / 2));
+   * $firstPart = substr($content, 0, strlen($secondPart)-1);
+   * // Open the file in writing mode
+   * $file = fopen($filePath, '+r');
+   * //move the cursor to the first content part length
+   * fseek($file, strlen($firstPart), SEEK_SET);
+   * //write the reversed second part into the file (strrev)
+   * fwrite($file, strrev($secondPart), strlen($secondPart));
+   * fclose($file);
+   * }
+   */
 }
 
 easterReverse();
